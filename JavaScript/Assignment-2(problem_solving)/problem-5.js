c8fc85556bf64a4a1ffe259ef6d379ce6596d8bd
Shamim-Islam <shamimislam844gm@gmail.com>
2026-07-31 01:19:21 +0600

    updated

--- JavaScript/Assignment-2(problem_solving)/problem-5.js
+++ JavaScript/Assignment-2(problem_solving)/problem-5.js
@@ -1,2 +1,23 @@
 // Problem 5 —Debugging Challenge: API Response Time Monitor
 
+function averageResponseTime(times) {
+  if (Array.isArray(times) === false || times.length === 0) {
+    return "Invalid";
+  }
+
+  let total = 0;
+  for (let i = 0; i < times.length; i++) {
+    if(typeof times[i] !== 'number') {
+        return "Invalid";
+    }
+    total += times[i];
+  }
+
+  return total / times.length;
+}
+
+console.log(averageResponseTime([120, 200, 150, 130]));
+console.log(averageResponseTime([100, 100]));
+console.log(averageResponseTime([]));
+console.log(averageResponseTime("logs"));
+console.log(averageResponseTime([120, "200", 150]));
